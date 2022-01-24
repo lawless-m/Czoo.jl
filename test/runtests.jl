8e353e6d224e5763bb425c87cb274942c81525a3
lawless-m <matt-github@steponnopets.net>
2022-01-24 09:24:28 +0000

    guard tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,14 @@ using Test
 @testset "Czoo.jl" begin
     cd("..")
     read(`make src/`)
-    # Write your tests here.
-    @test free_ptr(C_NULL) == nothing
-    @test add(1, 2) == 3
-    @test concat("a", "b") == "ab"
-    @test cons("a", "b") == ["a", "b"]
-    @test pstring("aaz") == Pstring("aaz")
-    @test ptr_pstring("zaz") == Pstring("zaz")
-    @test isequal(linked_pstrings("a1", "b2", "c3"), LinkedPstring(LinkedPstring(LinkedPstring("c3"), "b2"), "a1"))
-    @test print_list_int(Cint[1,2,3]) == 24
+    if isfile("src/libczoo.so")
+        @test free_ptr(C_NULL) == nothing
+        @test add(1, 2) == 3
+        @test concat("a", "b") == "ab"
+        @test cons("a", "b") == ["a", "b"]
+        @test pstring("aaz") == Pstring("aaz")
+        @test ptr_pstring("zaz") == Pstring("zaz")
+        @test isequal(linked_pstrings("a1", "b2", "c3"), LinkedPstring(LinkedPstring(LinkedPstring("c3"), "b2"), "a1"))
+        @test print_list_int(Cint[1,2,3]) == 24
+    end
 end
